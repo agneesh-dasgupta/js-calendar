39740a81db02614f0a6103717e1b5fc0dfb73770
Danny Aguirre <daniel.aguirreduran@wustl.edu>
2018-03-14 22:38:51 -0500

    chnages to login

--- login_ajax.php
+++ login_ajax.php
@@ -3,22 +3,21 @@ require 'database.php';
 header("Content-Type: application/json"); // Since we are sending a JSON response here (not an HTML document), set the MIME Type to application/json
 $username = $_POST['username'];
 $password = $_POST['password'];
-$stmt = $mysqli->prepare("SELECT COUNT(*), username, password FROM users WHERE username=?");
+$stmt = $mysqli->prepare("select username, password FROM users WHERE username=?");
 // Bind the parameter
-$stmt->bind_param('s', $user);
-$user = $_POST['username'];
+$stmt->bind_param('s', $username);
+//$user = $_POST['username'];
 //$isGuest = (string) $_POST['isGuest'];
 //$_SESSION['isGuest'] = $isGuest;
 $stmt->execute();
 
-$stmt->bind_result($cnt, $user_id, $pwd_hash);
+$stmt->bind_result($user_id, $pwd_hash);
 $stmt->fetch();
-$pwd_guess = $_POST['password'];
 
-if($cnt == 1 && password_verify($pwd_guess, $pwd_hash)){
+if(password_verify($password, $pwd_hash)){
 	// Login succeeded!
 	$_SESSION['username'] = $username;
-	$_SESSION['token'] = substr(md5(rand()), 0, 10);
+	//$_SESSION['token'] = substr(md5(rand()), 0, 10);
 	echo json_encode(array(
 		"success" => true
 	));
