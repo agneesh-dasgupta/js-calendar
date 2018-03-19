25987cfe80cab66b24d87a48800532d233691a38
Agneesh Dasgupta <agneeshdasgupta@wustl.edu>
2018-03-18 22:02:33 -0500

    yo

--- showEvents.php
+++ showEvents.php
@@ -6,22 +6,30 @@ header("Content-Type: application/json");
 $username = $_SESSION['username'];
 $currentMonth = $_POST['currentMonth'];
 
-$stmt = $mysqli->prepare("select eventTitle, eventMonth, eventDay from events where username=? and month=?");
+$stmt = $mysqli->prepare("select eventTitle, eventMonth, eventDay from events where username=? and eventMonth=?");
     if(!$stmt){
         printf("Query Prep Failed: %s\n", $mysqli->error);
         exit;
     }
-     $stmt->bind_param('sd', $username, $currentMonth);
+    $stmt->bind_param('sd', $username, $currentMonth);
     $stmt->execute();
     $stmt->bind_result($eventTitle, $eventMonth, $eventDay);
     $index =0;
-    $eventArray = array();
-    while($stmt->fetch()){
-		//check to see if username is unique
-        $eventArray = array (($eventMonth-1) + "-" + $eventDay, $eventTitle);
-        $index++;
-       
+    $result = $stmt->get_result();
+    $eventArray = array("success" => true);
+    while($row=$result->fetch_assoc()){
+       /* $event = array(
+            "eventMonth" => $row['eventMonth'],
+            "eventTitle" => $row['eventTitle'],
+            "eventDay" => $row['eventDay']
+        );
+        $eventarray[$index] = $event;
+        $index++;*/
     }
-    echo json_encode($eventArray);
-	$stmt->close();
+    $stmt->close();
+    echo json_encode(array(
+	"success" => true
+	));
+	exit;
+	
 ?>
